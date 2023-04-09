4c64170a06c98a9a804b893e08a19a9882956d30
Samrat <77772897+samrat2k03@users.noreply.github.com>
2023-04-09 10:08:17 +0530

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,14 +3,11 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA7M-aoiG78NIkrrr6v7Oy1gqEWRjcBo2Y",
-  authDomain: "student-hub-1.firebaseapp.com",
-  projectId: "student-hub-1",
-  storageBucket: "student-hub-1.appspot.com",
-  messagingSenderId: "345674055053",
-  appId: "1:345674055053:web:47720aa1b956b16ac091c4",
-  measurementId: "G-VTJLWQ9B98"
+
+  // put your firebase config
+
 };
+
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
